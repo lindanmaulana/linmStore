cd505b155f83a4951060e5b2f58cab2c2d5ceffc
dian ardiansyah <you@example.com>
2024-03-31 09:11:37 +0700

    update lagi bro

--- src/components/Elements/MostPopularCard/MostCard.jsx
+++ src/components/Elements/MostPopularCard/MostCard.jsx
@@ -15,7 +15,7 @@ const MostCard = (props) => {
   };
   return (
     <div className="snap-center snap-always">
-      <div className=" w-64 h-72 bg-blue-700 flex flex-col justify-center items-center gap-2 rounded-md overflow-hidden lg:w-80 lg:h-96">
+      <div className=" w-64 h-72 shadow-lg shadow-black flex flex-col justify-center items-center gap-2 rounded-md overflow-hidden lg:w-80 lg:h-96">
         <div
           className="relative w-full h-3/5 bg-cover bg-center"
           style={{ backgroundImage: `url(${bgDestination})` }}
@@ -38,23 +38,23 @@ const MostCard = (props) => {
         <div className="w-full flex justify-between px-2 py-2 border-2 divide-x-2">
           <div className="w-full flex justify-between px-2">
             <div className=" flex gap-2 self-center">
-              <a href="" className="text-xs self-center text-white">
+              <a href="" className="text-xs self-center text-black">
                 get
               </a>
-              <a href="" className="text-xs self-center text-white">
+              <a href="" className="text-xs self-center text-black">
                 about
               </a>
-              <a href="" className="text-xs self-center text-white">
+              <a href="" className="text-xs self-center text-black">
                 check
               </a>
             </div>
-            <div className="flex self-center text-white">
+            <div className="flex self-center text-black">
               <IoArrowRedoCircleOutline />
             </div>
           </div>
           <button
             onClick={handleClickStar}
-            className="flex self-center text-white gap-2 pl-2 "
+            className="flex self-center text-black gap-2 pl-2 "
           >
             {star ? <IoStarOutline /> : <IoStar className="text-blue-500" />}
           </button>
